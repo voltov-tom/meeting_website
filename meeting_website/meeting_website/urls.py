d071d320cdb7a619df73067fe0fc9542348967a0
voltov-tom <voltov-tom@mail.ru>
2022-02-23 14:00:15 +0400

    add 'if debug' static and media root

--- meeting_website/meeting_website/urls.py
+++ meeting_website/meeting_website/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
@@ -16,3 +18,6 @@ urlpatterns = [
     path('api/clients/create/', UserListView.as_view()),
 ]
 urlpatterns += router.urls
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
